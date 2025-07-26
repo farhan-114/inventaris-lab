5fba5d2f4be8a43a76e28c9ef440c1eb6f7ef43d
farhan <frhnsywl36@gmail.com>
2025-07-26 21:25:48 +0700

    Perbaiki DashboardController dan selesaikan conflict

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,15 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Barang;
-<<<<<<< HEAD
-use App\Models\BarangMasuk;
-use App\Models\Peminjaman;
-use App\Models\Stok;
-=======
-use App\Models\Peminjaman;
-use App\Models\Stok;
 use App\Models\Penerimaan;
->>>>>>> e7f83e930b536a4ebe305d3e34eec83f69936ad2
+use App\Models\Peminjaman;
+use App\Models\Stok;
 
 class DashboardController extends Controller
 {
@@ -19,16 +13,10 @@ class DashboardController extends Controller
     {
         return view('dashboard', [
             'jumlahBarang' => Barang::count(),
-<<<<<<< HEAD
-            'jumlahBarangMasuk' => BarangMasuk::count(),
-            'jumlahPeminjaman' => Peminjaman::count(),
-            'jumlahStok' => Stok::count(),
-=======
             'jumlahBarangMasuk' => Penerimaan::count(),
             'jumlahPeminjaman' => Peminjaman::count(),
             'jumlahStok' => Stok::count(),
             'peminjamanTerbaru' => Peminjaman::with('barang')->latest()->take(5)->get(),
->>>>>>> e7f83e930b536a4ebe305d3e34eec83f69936ad2
         ]);
     }
 }
